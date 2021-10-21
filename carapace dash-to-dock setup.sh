b08a5595aac76481971363814af44878fd7ed957
HackaN <hackan@bahnhof.se>
2021-10-21 18:08:23 +0200

    Fixes for Ubuntu 21.10

--- carapace dash-to-dock setup.sh
+++ carapace dash-to-dock setup.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 clear
-echo -e "
-	-----------------------------| Dash Setup |----------------------------------
-	
-	Before we start, let's introduce each other. I am a script written for your
-	convenience, I like to call myself Stardust. What's your name?
-	" 
-read name
-clear
-echo "
-	-----------------------------| Dash Setup |----------------------------------
-	
-	Awesome to meet you, $name :-D
-	"
-
 while true; do
 	read -p "
 	
-	Now with the pleasantries out of the way - let's make sure that dash is setup
+	Hello, I am stardust. I am a script written for your
+	convenience - let's make sure that dash is setup
 	the right way! All the tweaks we do here are accessible from the Dash to Dock
 	settings.
 	
@@ -46,6 +33,10 @@ gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme false
 echo "	→ Deactivate built in theme"
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
 echo "	→ Shrink the Dash"
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+echo "	→ No panel mode"
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide-mode 'FOCUS_APPLICATION_WINDOWS'
+echo "	→ Only hide when needed"
 gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed true
 echo "	→ Nobody likes super-small icons you can't see"
 gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DEFAULT'
@@ -56,9 +47,30 @@ echo "
 	
 	
 	"
+	
+	while true; do
+	read -p "
+	
+	If you are using Ubuntu Dock ( which you propably are if you didn't manually install dash-to-dock in Ubuntu) we need to allow the dock to have a custom theme.
+	If you are running dash-to-dock just press N here.
+	
+	Setup Ubuntu Dock? (y/n) " yn
+	case $yn in
+		[Yy]* ) echo; echo "
+		Here we go..."; echo; break;;
+		[Nn]* ) echo; echo "
+			That's it. The dash should look awesome now.
+			Enjoy this version of Carapace :-)"; echo; exit;;
+		* ) echo; echo "
+		! You must answer something I understand...  - Y or N"; echo;;
+	esac
+
+done
+	
+gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme true
+echo "	→ Applying custom theme"
+
 echo "
 	That's it. The dash should look awesome now.
-	Keep it real, $name, and enjoy this version of Carapace :-)
-	
-	Peace!
+	Enjoy this version of Carapace :-)
 	"
